116afc6035e7cfe78b42e3d6cc7f812401895787
Angelica <msaenzc06@gmail.com>
2026-03-18 04:07:27 -0500

    Se realiza ejercicio 1 tutoria 8

---
+++ ejercicosTutoria/ejercicio1Tutoria8/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+
+    public static void main(String[] args) throws Exception {
+        saludar("Juan");
+        saludar("María");
+    }
+    public static void saludar(String nombre) {
+        System.out.println("Hola " + nombre + ", bienvenido al sistema");
+    }
+}
