cfb921f06880b9f103be7dce473b02e73dcd6328
Angelica <msaenzc06@gmail.com>
2026-04-17 05:52:42 -0500

    Agrego ejercicio finalizado

--- unidad3/Clase1/Ejercicio1/src/App.java
+++ unidad3/Clase1/Ejercicio1/src/App.java
@@ -12,23 +12,31 @@ public class App {
         inventario.agregarProducto("Tablet apple", 2);
 
         // Eliminar producto
-
+        System.out.println("---- ELIMINAR PRODUCTO ----");
         try {
             inventario.eliminarProducto("Celular Samsung");
+            System.out.println("Producto eliminado con exito");
         } catch (ProductoNoEncontradoException e) {
             System.out.println("Error: " + e.getMessage());
         }
+        System.out.println();
 
         // Buscar producto
+        System.out.println("---- BUSCAR PRODUCTO ----");
         try {
-            System.out.println(inventario.buscarProducto("Laptop Huawei"));
+            System.out.println("Buscando producto...");
+            System.out.println(inventario.buscarProducto("Laptop Lenovo"));
         } catch (ProductoNoEncontradoException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
+        System.out.println();
+
         // Actualizar cantidad
+        System.out.println("---- ACTUALIZAR CANTIDAD ----");
         try {
             inventario.actualizarCantidad("Laptop Huawei", 1);
+            System.out.println("Cantidad actualizada correctamente");
         } catch (CantidadInvalidaException e){
             System.out.println("Error: " + e.getMessage());
         } catch (ProductoNoEncontradoException e) {
